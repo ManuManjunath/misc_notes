2d9a5c6baa0b72e0adbfafc16333af35e1033121
ManuManjunath <52904203+ManuManjunath@users.noreply.github.com>
2020-03-03 13:52:15 +0530

    Update Pandas.py

--- Python/Pandas.py
+++ Python/Pandas.py
@@ -46,15 +46,6 @@ df.groupby(['col_1']).sum()
 # select sum(numerical columns) from df group by col_1, col_2
 df.groupby(['col_1', 'col_2']).sum()
 
-# Index
-df.loc[row_index, col_index]
-# row_index is by default --> 0, 1, 2, ..
-# To specify a particular column as Index:
-df = read_csv("this_file.csv", index='this_column')
-# col_index is the column name
-# If column_index = ":" --> Get all columns
-df.iloc[10:10, [0,3]]
-# Gets rows 10 to 20 and columns # 1 and 4
 
 # Graphs
 # bar graph with counts by col_1
