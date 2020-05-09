28a5161e25da575564900aa180310a0b7c010d31
ManuManjunath <52904203+ManuManjunath@users.noreply.github.com>
2020-05-09 20:05:08 +0530

    Delete slack_webhook.sh

--- Shell/slack_webhook.sh
+++
@@ -1,5 +0,0 @@
-# To create a new webhook, to to your slack channel and type - 
-# /webhook create
-# Use the webhook link below - 
-
-curl -X POST 'https://api.enterprise.com/slack_events/v1/webhooks/xxx' -H 'Content-type: application/json ' -d '{"text": "My text message using curl"}'
